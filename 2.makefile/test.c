57cbf5a3d0c9f207ed5f45e12581c6756e1ff72e
Hongjin Li <vic.hong@rock-chips.com>
2023-05-05 18:18:29 +0800

    add new dir for makefile

---
+++ 2.makefile/test.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: test.c
+    > Author: LiHongjin
+    > Mail: 872648180@qq.com 
+    > Created Time: Fri May  5 18:20:53 2023
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world!\n");
+
+    return 0;
+}
